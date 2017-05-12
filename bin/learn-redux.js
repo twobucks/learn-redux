17974c683069bc1425cdd13419bf08af66bf1e8e
Dino Trojak <dinotrojak@gmail.com>
2017-05-12 12:22:45 +0200

    added basic example

---
+++ bin/learn-redux.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+require('../learn-redux').execute(process.argv.slice(2))
---
+++ learn-redux.js
@@ -0,0 +1,14 @@
+const learnyounode = require('workshopper-adventure')({
+  appDir: __dirname,
+  languages: ['en'],
+  header: require('workshopper-adventure/default/header'),
+  footer: require('workshopper-adventure/default/footer'),
+  fail: require('workshopper-adventure/default/fail'),
+  pass: require('workshopper-adventure/default/pass')
+})
+
+learnyounode.addAll([
+  'HELLO WORLD'
+])
+
+module.exports = learnyounode
